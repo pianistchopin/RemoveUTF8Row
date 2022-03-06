d069ebe6bc3b0a17f2536b5bd250ad2e232808fe
Thomas Peters <pianist.chopin.1@gmail.com>
2022-03-07 06:57:27 +0800

    first commit

---
+++ patentrename/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace patentrename
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.runBt = new System.Windows.Forms.Button();
+            this.sourcePathTb = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sourcePathBt = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.destinationPathTb = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.desBrowseBtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.processingStatusLb = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // runBt
+            // 
+            this.runBt.Location = new System.Drawing.Point(465, 278);
+            this.runBt.Name = "runBt";
+            this.runBt.Size = new System.Drawing.Size(75, 23);
+            this.runBt.TabIndex = 0;
+            this.runBt.Text = "Run";
+            this.runBt.UseVisualStyleBackColor = true;
+            this.runBt.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // sourcePathTb
+            // 
+            this.sourcePathTb.Location = new System.Drawing.Point(60, 93);
+            this.sourcePathTb.Name = "sourcePathTb";
+            this.sourcePathTb.Size = new System.Drawing.Size(399, 20);
+            this.sourcePathTb.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Source";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // sourcePathBt
+            // 
+            this.sourcePathBt.Location = new System.Drawing.Point(465, 90);
+            this.sourcePathBt.Name = "sourcePathBt";
+            this.sourcePathBt.Size = new System.Drawing.Size(75, 23);
+            this.sourcePathBt.TabIndex = 3;
+            this.sourcePathBt.Text = "browse";
+            this.sourcePathBt.UseVisualStyleBackColor = true;
+            this.sourcePathBt.Click += new System.EventHandler(this.directoryPathBt_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(186, 45);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 49;
+            this.pictureBox1.TabStop = false;
+            // 
+            // destinationPathTb
+            // 
+            this.destinationPathTb.Location = new System.Drawing.Point(60, 153);
+            this.destinationPathTb.Name = "destinationPathTb";
+            this.destinationPathTb.Size = new System.Drawing.Size(399, 20);
+            this.destinationPathTb.TabIndex = 52;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 53;
+            this.label2.Text = "destination";
+            // 
+            // desBrowseBtn
+            // 
+            this.desBrowseBtn.Location = new System.Drawing.Point(465, 151);
+            this.desBrowseBtn.Name = "desBrowseBtn";
+            this.desBrowseBtn.Size = new System.Drawing.Size(75, 23);
+            this.desBrowseBtn.TabIndex = 54;
+            this.desBrowseBtn.Text = "browse";
+            this.desBrowseBtn.UseVisualStyleBackColor = true;
+            this.desBrowseBtn.Click += new System.EventHandler(this.desBrowseBtn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 13);
+            this.label3.TabIndex = 55;
+            this.label3.Text = "Processing status: ";
+            // 
+            // processingStatusLb
+            // 
+            this.processingStatusLb.AutoSize = true;
+            this.processingStatusLb.Location = new System.Drawing.Point(163, 196);
+            this.processingStatusLb.Name = "processingStatusLb";
+            this.processingStatusLb.Size = new System.Drawing.Size(0, 13);
+            this.processingStatusLb.TabIndex = 56;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 313);
+            this.Controls.Add(this.processingStatusLb);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.desBrowseBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.destinationPathTb);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.sourcePathBt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sourcePathTb);
+            this.Controls.Add(this.runBt);
+            this.Name = "Form1";
+            this.Text = "Patent Rename Tool";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button runBt;
+        private System.Windows.Forms.TextBox sourcePathTb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button sourcePathBt;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox destinationPathTb;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button desBrowseBtn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label processingStatusLb;
+    }
+}
+
---
+++ patentrename/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ExcelDataReader;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+
+namespace patentrename
+{
+    public partial class Form1 : Form
+    {
+        string source_path = "";
+        string destination_path = "";
+        int mDelay = 1000;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string default_str = @"";
+            
+            if (source_path.Equals(""))
+            {
+                MessageBox.Show("Please select source folder");
+                return;
+            }
+
+            if (destination_path.Equals(""))
+            {
+                MessageBox.Show("Please select destination folder");
+                return;
+            }
+
+            source_path = default_str + source_path;
+            destination_path = default_str + destination_path;
+
+            DirectoryInfo sourcePathInfo = new DirectoryInfo(source_path);
+            FileInfo[] source_file_list = sourcePathInfo.GetFiles();
+
+            foreach (FileInfo source_file in source_file_list)
+            {
+                string file_source_path = source_file.FullName;
+                string file_extension = source_file.Extension;
+
+                string fileNameWithoutExt = Path.GetFileNameWithoutExtension(source_file.FullName);
+                string file_des_path = destination_path + "\\" +  fileNameWithoutExt + "__p" + source_file.Extension;
+
+                if (file_extension == ".xlsx" || file_extension == ".xls")
+                {
+                    processingStatusLb.Text = file_source_path + " ---> " + file_des_path;
+                    convertExcel(file_source_path, file_des_path);
+                }
+            }
+
+            MessageBox.Show("The Work Done");
+        }
+
+        public void convertExcel(string file_source_path, string file_des_path)
+        {
+            //read source file----------------
+            FileStream r_source_filestream = File.Open(file_source_path, FileMode.Open, FileAccess.Read);
+
+            // ExcelDataReader nuget package to read excel
+            IExcelDataReader excelReader = ExcelReaderFactory.CreateReader(r_source_filestream);
+            DataSet result = excelReader.AsDataSet();
+
+            //destination new file-------------
+            var w_des_filestream = new FileStream(file_des_path, FileMode.Create, FileAccess.Write);
+
+            // NPOI nuget package to write excel
+            IWorkbook workbook;
+            workbook = new XSSFWorkbook();
+            ISheet excelSheet = workbook.CreateSheet("ResultSet");
+
+            int row_index = 0;
+            while (excelReader.Read())
+            {
+                IRow row = excelSheet.CreateRow(row_index);
+                bool check_field_haneng = true;
+
+                // tempRowList is 
+                // when any field sentence contain korean string, this all field, row has to removed
+                List<string> tempRowList = new List<string>();
+
+                for (int field_index = 0; field_index < excelReader.FieldCount; field_index++)
+                {
+                    string sentence = "";
+                    if (excelReader.GetString(field_index) != null)
+                        sentence = excelReader.GetString(field_index);
+                    if (!check_HanEng_korean(sentence))
+                    {
+                        tempRowList.Add(sentence);
+                    }
+                    else
+                    {
+                        check_field_haneng = false;
+                    }
+                }
+
+                //check row status for korean string
+                if (check_field_haneng)
+                {
+                    int field_index = 0;
+                    foreach (string tempRow in tempRowList)
+                    {
+                        row.CreateCell(field_index).SetCellValue(tempRow);
+                        field_index++;
+                    }
+                    row_index++;
+                }
+            }
+            workbook.Write(w_des_filestream);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //source folder browser ----------- < 
+        private void directoryPathBt_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+                source_path = fbd.SelectedPath;
+            else
+                source_path = string.Empty;
+            sourcePathTb.Text = source_path;
+        }
+        //source folder browser ----------- < 
+
+        public bool check_HanEng_korean(string sentence)
+        {
+            bool encoding = false;
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (isKor(sentence[i]) == true)
+                {
+                    encoding = true;
+                }
+            }
+
+            if (sentence.Contains("KOREA"))
+                encoding = true;
+
+            return encoding;
+        }
+
+        //check containing korean string----------- < 
+        public bool isKor(char ch)
+        {
+            if ((0xAC00 <= ch && ch <= 0xD7A3) || (0x3131 <= ch && ch <= 0x318E))
+                return true;
+            else
+                return false;
+        }
+        //check containing korean string----------- >
+
+
+        //destination folder browser ----------- < 
+        private void desBrowseBtn_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+                destination_path = fbd.SelectedPath;
+            else
+                destination_path = string.Empty;
+            destinationPathTb.Text = destination_path;
+        }
+        //destination folder browser ----------- < 
+    }
+}
